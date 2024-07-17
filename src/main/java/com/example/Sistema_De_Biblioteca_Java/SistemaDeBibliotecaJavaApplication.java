e0f51061034572ecf0c87568fd3a4b9f8f93b374
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-07-17 14:45:09 -0300

    Atualizando

---
+++ src/main/java/com/example/Sistema_De_Biblioteca_Java/SistemaDeBibliotecaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_De_Biblioteca_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBibliotecaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeBibliotecaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sistema_De_Biblioteca_Java/SistemaDeBibliotecaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_De_Biblioteca_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBibliotecaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
