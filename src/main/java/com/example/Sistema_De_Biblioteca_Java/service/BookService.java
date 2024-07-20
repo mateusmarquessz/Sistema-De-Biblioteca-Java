0462217106a690312c74b2db7a17e0704ca8aa25
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-07-20 13:31:34 -0300

    Primeiros metodos no service
    Lista todos os livros

--- src/main/java/com/example/Sistema_De_Biblioteca_Java/service/BookService.java
+++ src/main/java/com/example/Sistema_De_Biblioteca_Java/service/BookService.java
@@ -1,13 +1,28 @@
 package com.example.Sistema_De_Biblioteca_Java.service;
 
+import com.example.Sistema_De_Biblioteca_Java.entity.Book;
+import com.example.Sistema_De_Biblioteca_Java.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.Sistema_De_Biblioteca_Java.repository.BookRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+
+    public BookService(UserRepository userRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    //Lista todos os Livros
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 
     // Métodos para criar, atualizar, deletar, listar e encontrar livros
 }
