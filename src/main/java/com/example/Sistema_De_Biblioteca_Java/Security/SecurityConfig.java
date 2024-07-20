b8c670ce6be701484ecb8080b926e26ae9c2b6a4
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-07-19 23:32:08 -0300

    Desativando Spring Security

---
+++ src/main/java/com/example/Sistema_De_Biblioteca_Java/Security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.Sistema_De_Biblioteca_Java.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll() 
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+}
