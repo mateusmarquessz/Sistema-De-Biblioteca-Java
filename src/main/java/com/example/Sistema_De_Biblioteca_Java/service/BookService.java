a14be1677dcab36d71b0e3473bcb075f75622168
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-07-20 21:43:25 -0300

    Criando os metodos do Service

--- src/main/java/com/example/Sistema_De_Biblioteca_Java/service/BookService.java
+++ src/main/java/com/example/Sistema_De_Biblioteca_Java/service/BookService.java
@@ -1,5 +1,7 @@
 package com.example.Sistema_De_Biblioteca_Java.service;
 
+import Exceptions.BookNotFoundException;
+import Exceptions.ResourceNotFoundException;
 import com.example.Sistema_De_Biblioteca_Java.entity.Book;
 import com.example.Sistema_De_Biblioteca_Java.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -22,6 +24,38 @@ public class BookService {
     //Lista todos os Livros
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    //Livros pelo ID
+    public Book getBookById(long id) {
+        return bookRepository.findById(id).
+                orElseThrow(() -> new BookNotFoundException("Book not found with id: "+ id));
+    }
+
+    //Cria Livro no Banco
+    public Book createBook(Book book) {
+        book.setCreatedAt(LocalDateTime.now());
+        return bookRepository.save(book);
+    }
+
+    //Deleta Livro do Banco
+    @Transactional
+    public void deleteBookById(long id) {
+        bookRepository.deleteById(id);
+    }
+
+    //Atualiza Book
+    public Book updateBook(Book book, long id) {
+        bookRepository.findById(id).
+                orElseThrow(()-> new ResourceNotFoundException("Book not found with id: "+ id));
+
+        book.setName(book.getName());
+        book.setCategory(book.getCategory());
+        book.setIsbn(book.getIsbn());
+        book.setQtdPages(book.getQtdPages());
+        book.setEdition(book.getEdition());
+
+        return bookRepository.save(book);
     }
 
     // Métodos para criar, atualizar, deletar, listar e encontrar livros
